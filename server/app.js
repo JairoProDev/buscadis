63c5dd8a2292e368c113184de24e397d9670b3bb
Jairo Saul <JairoProDev@gmail.com>
2024-02-01 20:35:37 -0500

    cambie la ruta de acceso a client, quitando __dirname y cambiando join por resolve

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'client/build'))); // Servir archivos estáticos
+app.use(express.static(path.resolve(__dirname, '../client/build'))); // Servir archivos estáticos
 
 // MongoDB connection with mongoose
 mongoose.connect(process.env.MONGODB_URI, {
@@ -33,7 +33,7 @@ app.listen(PORT, () => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 // Error handling middleware
