24e09f45d5b5df5f32949fd5c616269e2278e225
Jairo Saul <JairoProDev@gmail.com>
2024-02-02 14:22:32 -0500

    Add styles and components to MainComponent

--- client/src/MainComponent.js
+++ client/src/MainComponent.js
@@ -1,4 +1,23 @@
+import "./styles/root.css";
+import "./styles/reset.css";
+import "./styles/body.css";
+
+import "./MainComponent.css";
+
+import "./styles/navigation.css";
+import "./styles/responsive.css";
+import "./styles/navbar.css";
+import "./styles/mainContent.css";
+import "./styles/PublishButton.css";
+
 import React, { Fragment } from "react";
+import AdList from "./components/AdList/AdList";
+import Sidebar from "./components/Sidebar/Sidebar";
+import AdForm from "./components/AdForm/AdForm";
+// import NavBar from './components/NavBar';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import Home from './pages/Home';
+import Header from "./components/Header/Header";
 import useAds from "./hooks/useAds";
 
 function MainComponent() {
@@ -6,18 +25,21 @@ function MainComponent() {
 
   return (
     <Fragment>
+      <Header />
       <div className="container">
         <div className="main-content">
+          <Sidebar />
+          <AdList anuncios={anuncios} />
           <ul>
             {anuncios.map((anuncio) => (
               <li key={anuncio._id}>{anuncio.title}</li>
             ))}
           </ul>
+          <AdForm agregarAnuncioAlPrincipio={agregarAnuncioAlPrincipio} />
           {error && <div className="error">{error}</div>}
         </div>
       </div>
     </Fragment>
   );
 }
-
 export default MainComponent;
