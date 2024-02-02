d5b221a135f91b8e894b50f4b68f4e908bf676fa
Jairo Saul <JairoProDev@gmail.com>
2024-02-02 14:16:05 -0500

    Remove unused imports and update rendering of ad list

--- client/src/MainComponent.js
+++ client/src/MainComponent.js
@@ -1,23 +1,4 @@
-import "./styles/root.css";
-import "./styles/reset.css";
-import "./styles/body.css";
-
-import "./MainComponent.css";
-
-import "./styles/navigation.css";
-import "./styles/responsive.css";
-import "./styles/navbar.css";
-import "./styles/mainContent.css";
-import "./styles/PublishButton.css";
-
 import React, { Fragment } from "react";
-import AdList from "./components/AdList/AdList";
-import Sidebar from "./components/Sidebar/Sidebar";
-import AdForm from "./components/AdForm/AdForm";
-// import NavBar from './components/NavBar';
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-// import Home from './pages/Home';
-import Header from "./components/Header/Header";
 import useAds from "./hooks/useAds";
 
 function MainComponent() {
@@ -25,16 +6,18 @@ function MainComponent() {
 
   return (
     <Fragment>
-      <Header />
       <div className="container">
         <div className="main-content">
-          <Sidebar />
-          <AdList anuncios={anuncios} />
-          <AdForm agregarAnuncioAlPrincipio={agregarAnuncioAlPrincipio} />
+          <ul>
+            {anuncios.map((anuncio) => (
+              <li key={anuncio._id}>{anuncio.title}</li>
+            ))}
+          </ul>
           {error && <div className="error">{error}</div>}
         </div>
       </div>
     </Fragment>
   );
 }
+
 export default MainComponent;
