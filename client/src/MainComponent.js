14f1d1306409d5a478f2d09ddefe07039f401c20
Jairo Saul <JairoProDev@gmail.com>
2024-02-03 14:27:22 -0500

    Add AdCard component and update MainComponent

--- client/src/MainComponent.js
+++ client/src/MainComponent.js
@@ -9,6 +9,11 @@ import "./styles/responsive.css";
 import "./styles/navbar.css";
 import "./styles/mainContent.css";
 import "./styles/PublishButton.css";
+import "./components/AdCard/adCard.css";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 import React, { Fragment } from "react";
 import AdList from "./components/AdList/AdList";
@@ -29,7 +34,24 @@ function MainComponent() {
       <div className="container">
         <div className="main-content">
           <Sidebar />
-          <AdList anuncios={anuncios} />
+          <div className="ad-card">
+            <div className="ad-card__content">
+                <h3 className="ad-card__title">{title}</h3>
+                <p className="ad-card__description">{description}</p>
+                <div className="ad-card__details">
+                    <p className="ad-card__price">{amount}</p>
+                    <p className="ad-card__location">{location}</p>
+                </div>
+                <div className="ad-card__buttons">
+                    <a href={`tel:${phone}`} className="ad-card__button ad-card__button--details" aria-label="Call">
+                        <FontAwesomeIcon icon={faPhone} />
+                    </a>
+                    <a href={`https://wa.me/${phone}?text=${encodeURIComponent("Hola, vi su anuncio en PublicAdis.com y me interesa, podría proporcionarme más información por favor?")}`} className="ad-card__button ad-card__button--contact" aria-label="Contact on WhatsApp">
+                        <FontAwesomeIcon icon={faWhatsapp} size='2x' />
+                    </a>
+                </div>
+            </div>
+        </div>
           <AdForm agregarAnuncioAlPrincipio={agregarAnuncioAlPrincipio} />
           {error && <div className="error">{error}</div>}
         </div>
