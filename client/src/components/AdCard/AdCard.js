ecac114e2910e2c4e8fad806281c21518d4735fd
Jairo Saul <JairoProDev@gmail.com>
2024-02-03 14:10:06 -0500

    Refactor AdCard component to handle missing data

--- client/src/components/AdCard/AdCard.js
+++ client/src/components/AdCard/AdCard.js
@@ -10,24 +10,20 @@ function AdCard({ anuncio }) {
     return (
         <div className="ad-card">
             <div className="ad-card__content">
-                {title && <h3 className="ad-card__title">{title}</h3>}
-                {description && <p className="ad-card__description">{description}</p>}
-                {amount && location && (
-                    <div className="ad-card__details">
-                        <p className="ad-card__price">{amount}</p>
-                        <p className="ad-card__location">{location}</p>
-                    </div>
-                )}
-                {phone && (
-                    <div className="ad-card__buttons">
-                        <a href={`tel:${phone}`} className="ad-card__button ad-card__button--details" aria-label="Call">
-                            <FontAwesomeIcon icon={faPhone} />
-                        </a>
-                        <a href={`https://wa.me/${phone}?text=${encodeURIComponent("Hola, vi su anuncio en PublicAdis.com y me interesa, podría proporcionarme más información por favor?")}`} className="ad-card__button ad-card__button--contact" aria-label="Contact on WhatsApp">
-                            <FontAwesomeIcon icon={faWhatsapp} size='2x' />
-                        </a>
-                    </div>
-                )}
+                <h3 className="ad-card__title">{title || 'Título no disponible'}</h3>
+                <p className="ad-card__description">{description || 'Descripción no disponible'}</p>
+                <div className="ad-card__details">
+                    <p className="ad-card__price">{amount || 'Cantidad no disponible'}</p>
+                    <p className="ad-card__location">{location || 'Ubicación no disponible'}</p>
+                </div>
+                <div className="ad-card__buttons">
+                    <a href={`tel:${phone || ''}`} className="ad-card__button ad-card__button--details" aria-label="Call">
+                        <FontAwesomeIcon icon={faPhone} />
+                    </a>
+                    <a href={`https://wa.me/${phone || ''}?text=${encodeURIComponent("Hola, vi su anuncio en PublicAdis.com y me interesa, podría proporcionarme más información por favor?")}`} className="ad-card__button ad-card__button--contact" aria-label="Contact on WhatsApp">
+                        <FontAwesomeIcon icon={faWhatsapp} size='2x' />
+                    </a>
+                </div>
             </div>
         </div>
     );
