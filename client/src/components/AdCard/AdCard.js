e7499731383e5c477ad5c2ec0db5f8a2e177882b
Jairo Saul <JairoProDev@gmail.com>
2024-02-04 13:07:08 -0500

    Add category to AdCard component

--- client/src/components/AdCard/AdCard.js
+++ client/src/components/AdCard/AdCard.js
@@ -7,7 +7,7 @@ import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 function AdCard({ anuncio }) {
     console.log('AdCard anuncio:', anuncio)
 
-    const { title, description, amount, location, phone } = anuncio;
+    const { category, title, description, amount, location, phone } = anuncio;
 
     return (
         <div className="ad-card">
@@ -15,17 +15,18 @@ function AdCard({ anuncio }) {
                 <h3 className="ad-card__title">{title}</h3>
                 <p className="ad-card__description">{description}</p>
                 <div className="ad-card__details">
+                    <p className="ad-card__category">{category}</p>
                     <p className="ad-card__price">{amount}</p>
                     <p className="ad-card__location">{location}</p>
                 </div>
-                <div className="ad-card__buttons">
-                    <a href={`tel:${phone}`} className="ad-card__button ad-card__button--details" aria-label="Call">
-                        <FontAwesomeIcon icon={faPhone} />
-                    </a>
-                    <a href={`https://wa.me/${phone}?text=${encodeURIComponent("Hola, vi su anuncio en PublicAdis.com y me interesa, podría proporcionarme más información por favor?")}`} className="ad-card__button ad-card__button--contact" aria-label="Contact on WhatsApp">
-                        <FontAwesomeIcon icon={faWhatsapp} size='2x' />
-                    </a>
-                </div>
+            </div>
+            <div className="ad-card__buttons">
+                <a href={`tel:${phone}`} className="ad-card__button ad-card__button--details" aria-label="Call">
+                    <FontAwesomeIcon icon={faPhone} />
+                </a>
+                <a href={`https://wa.me/${phone}?text=${encodeURIComponent("Hola, vi su anuncio en PublicAdis.com y me interesa, podría proporcionarme más información por favor?")}`} className="ad-card__button ad-card__button--contact" aria-label="Contact on WhatsApp">
+                    <FontAwesomeIcon icon={faWhatsapp} size='2x' />
+                </a>
             </div>
         </div>
     );
