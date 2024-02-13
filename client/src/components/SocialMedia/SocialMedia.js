368875d08c3f49277c94490d1d2ea6a55dc6113f
Jairo Saul <JairoProDev@gmail.com>
2024-02-12 22:54:59 -0500

    Update social media links in SocialMedia component

--- client/src/components/SocialMedia/SocialMedia.js
+++ client/src/components/SocialMedia/SocialMedia.js
@@ -13,38 +13,54 @@ import {
 const SocialMedia = () => {
   return (
     <div className="social-media">
-    <ul className="wrapper">
-      <li className="icon facebook">
-        <span className="tooltip">Facebook</span>
-        <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
-          <FontAwesomeIcon icon={faFacebookF} />
-        </a>
-      </li>
-      <li className="icon instagram">
-        <span className="tooltip">Instagram</span>
-        <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
-          <FontAwesomeIcon icon={faInstagram} />
-        </a>
-      </li>
-      <li className="icon linkedin">
-        <span className="tooltip">LinkedIn</span>
-        <a href="https://www.linkedin.com" target="_blank" rel="noreferrer">
-          <FontAwesomeIcon icon={faLinkedinIn} />
-        </a>
-      </li>
-      <li className="icon tiktok">
-        <span className="tooltip">TikTok</span>
-        <a href="https://www.tiktok.com" target="_blank" rel="noreferrer">
-          <FontAwesomeIcon icon={faTiktok} />
-        </a>
-      </li>
-      <li className="icon whatsapp">
-        <span className="tooltip">WhatsApp</span>
-        <a href="https://wa.me/937054328" target="_blank" rel="noreferrer">
-          <FontAwesomeIcon icon={faWhatsapp} />
-        </a>
-      </li>
-    </ul>
+      <ul className="wrapper">
+        <li className="icon facebook">
+          <span className="tooltip">Facebook</span>
+          <a
+            href="https://www.facebook.com/publicadis"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faFacebookF} />
+          </a>
+        </li>
+        <li className="icon instagram">
+          <span className="tooltip">Instagram</span>
+          <a
+            href="https://www.instagram.com/publicadis"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+        </li>
+        <li className="icon linkedin">
+          <span className="tooltip">LinkedIn</span>
+          <a
+            href="https://www.linkedin.com/JairoProDev"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedinIn} />
+          </a>
+        </li>
+        <li className="icon tiktok">
+          <span className="tooltip">TikTok</span>
+          <a
+            href="https://www.tiktok.com/publicadis"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faTiktok} />
+          </a>
+        </li>
+        <li className="icon whatsapp">
+          <span className="tooltip">WhatsApp</span>
+          <a href="https://wa.me/937054328" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faWhatsapp} />
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
