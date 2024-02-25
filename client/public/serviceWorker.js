7cf29681081bc8286dcabd0d8c82202643f53f62
Jairo Saul <JairoProDev@gmail.com>
2024-02-25 10:53:26 -0500

    Refactor serviceWorker.js to cache additional resources

--- client/public/serviceWorker.js
+++ client/public/serviceWorker.js
@@ -1,30 +1,25 @@
-/* eslint-env serviceworker */
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('my-cache').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                // Aquí puedes agregar más rutas y recursos para cachear
-            ]);
-        })
-    );
+  event.waitUntil(
+    caches.open('my-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/static/js/main.3da71462.js',
+        '/static/css/main.42bc8682.css',
+        // Agrega aquí todos los demás recursos que quieras almacenar en caché
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', event => {
-  if (event.request.url.endsWith('.js')) {
-    event.respondWith(
-      fetch(event.request)
-        .then(response => {
-          const responseClone = response.clone();
-          caches.open('my-cache').then(cache => cache.put(event.request, responseClone));
-          return response;
-        })
-        .catch(() => caches.match(event.request))
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request).then(response => response || fetch(event.request))
-    );
-  }
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).then(response => {
+        const responseClone = response.clone();
+        caches.open('my-cache').then(cache => cache.put(event.request, responseClone));
+        return response;
+      });
+    })
+  );
 });
