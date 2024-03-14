0ef720eb1d9c8650a76df0be6cfe6bcf4d97ebd0
Jairo Saul <JairoProDev@gmail.com>
2024-03-14 10:41:01 -0500

    Add Cloudinary configuration to imageRoutes.js

--- server/routes/imageRoutes.js
+++ server/routes/imageRoutes.js
@@ -4,6 +4,13 @@ const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
 const cloudinary = require('cloudinary').v2;
 
+// Configurar Cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
 router.post('/upload', upload.single('image'), async (req, res) => {
     if (!req.file) {
         res.json({ message: 'No image file was provided.' });
@@ -15,7 +22,8 @@ router.post('/upload', upload.single('image'), async (req, res) => {
         res.json({ imageUrl: result.secure_url });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Error uploading image', message: error.message });    }
+        res.status(500).json({ error: 'Error uploading image', message: error.message });
+    }
 });
 
 module.exports = router;
