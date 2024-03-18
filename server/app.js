5b312afb32e92ceb955f72b4d4eb8088531b0ff0
Jairo Saul <JairoProDev@gmail.com>
2024-03-18 17:48:37 -0500

    Add authentication routes and middleware

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
+// Importar las dependenc// Importar las dependencias necesarias
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "../.env" });
 }
 const cloudinary = require("cloudinary").v2;
 
+// Configurar Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
@@ -14,11 +16,11 @@ const mongoose = require("mongoose");
 const path = require("path");
 const adRoutes = require("./routes/adRoutes");
 const cors = require("cors");
-
 const imageRoutes = require("./routes/imageRoutes");
-// const userRoutes = require('./routes/userRoutes');
-// const authRoutes = require('./routes/authRoutes');
-
+const authRoutes = require('./routes/authRoutes');
+const expressJwt = require('express-jwt');
+console.log(expressJwt);
+// const authenticateToken = require('./middlewares/authenticateToken');
 // Inicializar la aplicación
 const app = express();
 
@@ -34,6 +36,7 @@ app.use(express.json());
 // Usar middleware para servir archivos estáticos
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
+// Usar las rutas de imágenes
 app.use("/api/images", imageRoutes);
 
 // Contador para los intentos de conexión a la base de datos
@@ -74,10 +77,22 @@ connectToDb();
 
 // Usar las rutas de la API
 app.use("/api", adRoutes);
+app.use('/api/auth', authRoutes);
 
-// Iniciar el servidor
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+// Usa el middleware de autenticación antes de las rutas protegidas
+// app.use('/api/auth/protected-route', expressJwt({ secret: process.env.JWT_SECRET, algorithms: ['HS256'] }));
+app.get('/api/auth/protected-route', (req, res) => {
+  // Esta ruta está protegida. Solo los usuarios autenticados pueden acceder a ella.
+  res.json({ message: 'Esta es una ruta protegida' });
+});
+
+// Manejar todas las demás rutas enviando el archivo index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Ruta no encontrada' });
 });
 
 // Error handling middleware
@@ -88,7 +103,25 @@ app.use((err, req, res, next) => {
     .json({ error: err.message || "Error interno del servidor" });
 });
 
-// Manejar todas las demás rutas enviando el archivo index.html
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
+// Este es el archivo principal de nuestra aplicación Express.js, app.js. Aquí es donde configuramos nuestra aplicación y definimos nuestras rutas.
+
+// Primero, verificamos si estamos en un entorno de desarrollo. Si es así, utilizamos el módulo dotenv para cargar nuestras variables de entorno desde el archivo .env.
+
+// Luego, importamos y configuramos cloudinary, que es el servicio que utilizamos para almacenar y servir imágenes. Las credenciales para cloudinary se almacenan en nuestras variables de entorno.
+
+// Después de eso, importamos los módulos que necesitamos para nuestra aplicación. Esto incluye express para crear nuestra aplicación, mongoose para trabajar con MongoDB, path para trabajar con rutas de archivos, cors para permitir solicitudes CORS, y nuestras rutas personalizadas.
+
+// Inicializamos nuestra aplicación Express y establecemos el puerto en el que se ejecutará. Luego, configuramos algunos middlewares: cors para permitir solicitudes CORS, express.json para parsear el cuerpo de las solicitudes JSON, y express.static para servir nuestros archivos estáticos.
+
+// A continuación, definimos nuestras rutas. Tenemos rutas para las imágenes, los anuncios y la autenticación. También tenemos una ruta protegida que solo puede ser accedida por usuarios autenticados.
+
+// Para conectar a nuestra base de datos MongoDB, utilizamos una función connectToDb que intenta conectarse a la base de datos y se reintentará hasta 5 veces si la conexión falla.
+
+// Finalmente, manejamos todas las demás rutas enviando el archivo index.html, y configuramos un middleware de manejo de errores para manejar cualquier error que pueda ocurrir en nuestra aplicación.
+
+// Una vez que todo está configurado, iniciamos nuestro servidor con app.listen.
