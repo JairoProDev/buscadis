b197f70b3f977f4ee2662d6aaa7277c09e08b870
Jairo Saul <JairoProDev@gmail.com>
2024-03-18 17:19:14 -0500

    Add User model with username, email, password, and tokens

---
+++ server/models/User.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid');
+            }
+        },
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true,
+        }
+    }]
+}, {
+    timestamps: true
+});
+
+// Hash the plain text password before saving
+userSchema.pre('save', async function (next) {
+    const user = this;
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+
+    next();
+});
+
+module.exports = mongoose.model('User', userSchema);
