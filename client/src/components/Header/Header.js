c6e85b4a13a375bfe7aa677f3412395a89aa0810
Jairo Saul <JairoProDev@gmail.com>
2024-03-19 10:27:42 -0500

    Refactor Header component

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -9,7 +9,12 @@ import SearchBar from "../SearchBar/SearchBar";
 import UserMenu from "../UserMenu/UserMenu";
 import AdForm from "../AdForm/AdForm"; // Importa el formulario de publicación del anuncio
 
-function Header({ openLoginModal, openRegisterModal, setFilter, toggleSidebar }) {
+function Header({
+  openLoginModal,
+  openRegisterModal,
+  setFilter,
+  toggleSidebar,
+}) {
   const isHidden = useScroll();
   const [isFormVisible, setIsFormVisible] = useState(false);
   const showForm = () => setIsFormVisible(true); // Esta es la única declaración de showForm que necesitas
@@ -23,11 +28,12 @@ function Header({ openLoginModal, openRegisterModal, setFilter, toggleSidebar })
         </button>*/}
         <Logo />
         {/* <SearchBar /> */}
-        <NavList
-
-          setFilter={setFilter}
-        />{" "}
-      <UserMenu openLoginModal={openLoginModal} openRegisterModal={openRegisterModal} toggleForm={showForm} />
+        <NavList setFilter={setFilter} />{" "}
+        <UserMenu
+          openLoginModal={openLoginModal}
+          openRegisterModal={openRegisterModal}
+          toggleForm={showForm}
+        />
       </div>
       {isFormVisible && <AdForm hideForm={hideForm} />}{" "}
       {/* Muestra el formulario si isFormVisible es true */}
@@ -39,7 +45,7 @@ function Logo({ setFilter }) {
   return (
     <Link to="/" className="header-left" onClick={() => setFilter(null)}>
       <img src={logo} alt="Logo" className="logo" />
-      <div className="header-title">PublicAdis</div>
+      <h1 className="header-title">PublicAdis</h1>
     </Link>
   );
 }
