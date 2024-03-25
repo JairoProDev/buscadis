757217b3ff10da9bad54be9e8a8bd3e6d5e68f86
Jairo Saul <JairoProDev@gmail.com>
2024-03-25 08:26:00 -0500

    Actualizé AuthContext.js con cambios en la importación y la lógica de autenticación

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,27 +1,48 @@
 // context/AuthContext.js
-import React, { createContext, useState, useEffect } from 'react';
-import { jwtDecode } from 'jwt-decode'; // Aquí cambiamos la importación
-
+import React, { createContext, useState, useEffect } from "react";
+import { jwtDecode } from 'jwt-decode';
 export const AuthContext = createContext();
 
-export const AuthProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null);
 
-    useEffect(() => {
-        // Supongamos que estás guardando el token en localStorage
-        const token = localStorage.getItem('token');
-        console.log('Token:', token);
-
-        if (token) {
-            const decodedToken = jwtDecode(token); // Aquí cambiamos jwt_decode a jwtDecode
-            console.log('Decoded token:', decodedToken);
-            setCurrentUser(decodedToken);
-        }
-    }, []);
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    console.log('Token:', token);
+  
+    let decodedToken;
+    if (token) {
+      decodedToken = jwtDecode(token);
+      console.log('Decoded token:', decodedToken);
+      setCurrentUser(decodedToken);
+    }
+    const fetchUser = async () => {
+        try {
+          const response = await fetch(`/api/auth/users/${decodedToken.id}`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+      
+              if (!response.ok) {
+                throw new Error("Error al cargar la información del usuario");
+              }
+      
+              const data = await response.json();
+              setCurrentUser(data);
+            } catch (error) {
+              console.error(error);
+            }
+          };
+      
+          if (decodedToken && decodedToken.id) {
+            fetchUser();
+          }
+        }, []);
 
     return (
-        <AuthContext.Provider value={{ currentUser }}>
-            {children}
+        <AuthContext.Provider value={{ currentUser, setCurrentUser }}>
+        {children}
         </AuthContext.Provider>
     );
 };
