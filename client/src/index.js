e175cb10f452340d198f032945f7a3bc84693cbe
Jairo Saul <JairoProDev@gmail.com>
2024-03-30 11:06:25 -0500

    Cambiar importación de ReactDOM a createRoot en index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 // Espera a que el DOM esté completamente cargado antes de renderizar la aplicación
 document.addEventListener("DOMContentLoaded", () => {
-  const root = ReactDOM.createRoot(document.getElementById("root"));
+  const root = createRoot(document.getElementById("root")); // Cambia esta línea
   root.render(
     <React.StrictMode>
       <App />
