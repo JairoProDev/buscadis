5a2bd3f1b700e1efcdb4b0682d6b4c5be9945f99
Jairo Saul <JairoProDev@gmail.com>
2024-04-02 11:39:43 -0500

    Agregado componente AdModal y habilitada ruta para anuncios

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainComponent from './MainComponent';
 import UserProfile from './components/UserProfile/UserProfile';
 import { AuthProvider } from './context/AuthContext';
-// import AdModal from './components/AdModal/AdModal';
+import AdModal from './components/AdModal/AdModal';
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <Routes>
           <Route path="/profile" element={<UserProfile />} />
           <Route path="/*" element={<MainComponent />} />
-          {/* <Route path="/anuncio/:id" element={<AdModal />} /> */}
+          <Route path="/anuncio/:id" element={<AdModal />} />
         </Routes>
       </AuthProvider>
     </Router>
