e0121c753f2adc5b3c0ae707201c472392950093
Jairo Saul <JairoProDev@gmail.com>
2024-04-11 10:38:13 -0500

    Update ContactButtons component to include category in ContactButton and send a customized messsage via whatsapp

--- client/src/components/ContactButtons/ContactButtons.js
+++ client/src/components/ContactButtons/ContactButtons.js
@@ -4,12 +4,28 @@ import { faPhone } from "@fortawesome/free-solid-svg-icons";
 import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 import "./contactButtons.css";
 
-function ContactButton({ phone, type }) {
+function ContactButton({ phone, type, category }) {
   const isWhatsApp = type === "whatsapp";
-  const href = isWhatsApp
-    ? `https://wa.me/${phone}?text=${encodeURIComponent(
-        "Hola, vi su aviso en BuscAdis.com y me interesa, podría proporcionarme más información por favor?"
-      )}`
+
+  let message;
+  switch (category) {
+    case "Autos":
+      message = "¡Hola! Vi su aviso en BuscAdis.com sobre el auto que tiene en venta y estoy interesado. ¿Podría proporcionarme más información, por favor?";
+      break;
+    case "Empleos":
+      message = "Buen día, encontré su anuncio en BuscAdis.com y estoy muy interesado en el empleo que ofrece. ¿Podría proporcionarme más detalles al respecto?";
+      break;
+    case "Inmuebles":
+      message = "Hola, vi su aviso en BuscAdis.com sobre la propiedad en venta y me interesa. ¿Podría darme más información sobre la misma?";
+      break;
+    case "Servicios":
+      message = "¡Hola! Vi su anuncio en BuscAdis.com y estoy interesado en los servicios que ofrece. ¿Podría proporcionarme más detalles al respecto?";
+      break;
+    default:
+      message = "Buen día, vi su aviso en BuscAdis.com y me interesa, podría proporcionarme más información por favor?";
+  }
+const href = isWhatsApp
+    ? `https://wa.me/${phone}?text=${encodeURIComponent(message)}`
     : `tel:${phone}`;
   const icon = isWhatsApp ? faWhatsapp : faPhone;
   const label = isWhatsApp ? "Contactar por WhatsApp" : "Llamar";
@@ -27,19 +43,19 @@ function ContactButton({ phone, type }) {
   );
 }
 
-function ContactButtons({ phone, phone2 }) {
+function ContactButtons({ phone, phone2, category }) {
   if (!phone && !phone2) return null;
 
   return (
     <>
       <div>
-        {phone && <ContactButton phone={phone} type="call" />}
-        {phone && <ContactButton phone={phone} type="whatsapp" />}
+        {phone && <ContactButton phone={phone} type="call" category={category}/>}
+        {phone && <ContactButton phone={phone} type="whatsapp" category={category} />}
       </div>
       <p>🌐BuscAdis.com🔗</p>
       <div>
-        {phone2 && <ContactButton phone={phone2} type="call" />}
-        {phone2 && <ContactButton phone={phone2} type="whatsapp" />}
+        {phone2 && <ContactButton phone={phone2} type="call" category={category} />}
+        {phone2 && <ContactButton phone={phone2} type="whatsapp" category={category} />}
       </div>
     </>
   );
