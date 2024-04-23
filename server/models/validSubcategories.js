71ec186f66abe0b509d8af3b031c83effa7c858a
Jairo Saul <JairoProDev@gmail.com>
2024-04-23 15:06:17 -0500

    agregu[e la subcategorría Otros para Empleos]

--- server/models/validSubcategories.js
+++ server/models/validSubcategories.js
@@ -1,6 +1,6 @@
 module.exports = {
     Vehículos: ["Autos", "SUVs", "Motos", "Camionetas", "Camiones", "Maquinaria Pesada", "Buses", "Vehículos Eléctricos", "Otros"],
-    Empleos: ["Hogar", "Hostelería","Turismo","Educación","Salud y Bienestar","Servicio al Cliente","Administración y Oficina","Ventas","Operaciones y Logística","Tecnología y Digitales","Construcción y Mantenimiento","Prácticas y Becarios","Artes y Creatividad","Sector Financiero"],
+    Empleos: ["Hogar", "Hostelería","Turismo","Educación","Salud y Bienestar","Servicio al Cliente","Administración y Oficina","Ventas","Operaciones y Logística","Tecnología y Digitales","Construcción y Mantenimiento","Prácticas y Becarios","Artes y Creatividad","Sector Financiero", "Otros"],
     Inmuebles: ["Casas", "Habitaciones", "MiniDepartamentos", "Departamentos", "Terrenos", "Locales Comerciales", "Oficinas", "Edificios", "Almacenes", "Hoteles", "Hostales", "Otros"],
     Servicios: ["Salud", "Educación", "Mantenimiento", "Transporte", "Eventos", "Tecnología", "Otros"],
     Eventos: ["Culturales", "Deportivos", "Religiosos", "Académicos", "Empresariales", "Festivos", "Otros"],
