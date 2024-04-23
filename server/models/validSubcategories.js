ca7c4ed1b728d4f107ab9fa34d6f252b797c23c0
Jairo Saul <JairoProDev@gmail.com>
2024-04-22 20:13:29 -0500

    Add validSubcategories.js file with predefined subcategories for each category

---
+++ server/models/validSubcategories.js
@@ -0,0 +1,12 @@
+module.exports = {
+    Vehículos: ["Autos", "SUVs", "Motos", "Camionetas", "Camiones", "Maquinaria Pesada", "Buses", "Vehículos Eléctricos", "Otros"],
+    Empleos: ["Hogar", "Hostelería","Turismo","Educación","Salud y Bienestar","Servicio al Cliente","Administración y Oficina","Ventas","Operaciones y Logística","Tecnología y Digitales","Construcción y Mantenimiento","Prácticas y Becarios","Artes y Creatividad","Sector Financiero"],
+    Inmuebles: ["Casas", "Habitaciones", "MiniDepartamentos", "Departamentos", "Terrenos", "Locales Comerciales", "Oficinas", "Edificios", "Almacenes", "Hoteles", "Hostales", "Otros"],
+    Servicios: ["Salud", "Educación", "Mantenimiento", "Transporte", "Eventos", "Tecnología", "Otros"],
+    Eventos: ["Culturales", "Deportivos", "Religiosos", "Académicos", "Empresariales", "Festivos", "Otros"],
+    Educación: ["Inicial", "Primaria", "Secundaria", "Superior", "Postgrado", "Otros"],
+    Turismo: ["Aventura", "Cultural", "Gastronómico", "Religioso", "Ecoturismo", "Deportivo", "Negocios", "Otros"],
+    Productos: ["Alimentos", "Bebidas", "Ropa", "Calzado", "Electrodomésticos", "Muebles", "Juguetes", "Libros", "Otros"],
+    Negocios: ["Restaurantes", "Hoteles", "Hostales", "Bares", "Cafeterías", "Panaderías", "Farmacias", "Minimarkets", "Otros"],
+    Otros: ["Compra", "Venta", "Alquiler", "Servicio", "Evento", "Educación", "Turismo", "Producto", "Negocio", "Empleo"],
+  };
