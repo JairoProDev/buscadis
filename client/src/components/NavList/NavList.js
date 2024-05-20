fdece81ca50161cf0fc44f8998cb11cf388cf36b
Jairo Saul <JairoProDev@gmail.com>
2024-05-20 15:49:33 -0500

    refactor: Update NavList component to use icons for navigation items

--- client/src/components/NavList/NavList.js
+++ client/src/components/NavList/NavList.js
@@ -2,34 +2,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./navList.css";
+import JobsIcon from '../../icons/jobs.png';
+import EstateIcon from '../../icons/estate.png';
+import CarsIcon from '../../icons/vehicles.png';
+import ServiceIcon from '../../icons/services.png';
+import PublishIcon from '../../icons/publish.png';
 
 function NavList({ setFilter, toggleForm }) {
   return (
     <nav className="nav">
       <ul className="nav-list">
         <NavItem
-          icon="work"
+          icon={JobsIcon}
           link="/Empleos"
           label="Empleos"
           onClick={() => setFilter("Empleos")}
         />
         <NavItem
-          icon="house"
+          icon={EstateIcon}
           link="/Inmuebles"
           label="Inmuebles"
           onClick={() => setFilter("Inmuebles")}
         />
-
-        <NavItem icon="campaign" onClick={toggleForm} label="Anunciar" />
-
         <NavItem
-          icon="directions_car"
+          icon={PublishIcon}
+          onClick={toggleForm}
+          label="Anunciar"
+        />
+        <NavItem
+          icon={CarsIcon}
           link="/Autos"
           label="Autos"
           onClick={() => setFilter("Autos")}
         />
         <NavItem
-          icon="engineering"
+          icon={ServiceIcon}
           link="/Servicios"
           label="Servicios"
           onClick={() => setFilter("Servicios")}
@@ -48,8 +55,10 @@ function NavItem({ icon, link, label, onClick }) {
         onClick();
       }}  
     >
-      <span className="material-symbols-outlined">{icon}</span>
-      <Link to={link}>{label}</Link>
+      <img src={icon} alt="" className="nav-item-icon" />
+      <Link to={link}>
+        <div>{label}</div> {/* Agrega el nombre debajo del icono */}
+      </Link>
     </li>
   );
 }
