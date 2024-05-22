64d0a5eb357cb47f8c5e0dc5c9609579b7baeee8
Jairo Saul <JairoProDev@gmail.com>
2024-05-22 11:36:38 -0500

    refactor: Add new routes for getting, updating, and deleting ads by ID

--- server/routes/adRoutes.js
+++ server/routes/adRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { createAd, getAds } = require("../controllers/adController");
+const { createAd, getAds, getAdById, updateAd, deleteAd, getAdsByCategory } = require("../controllers/adController");
 
 router.post("/anuncios", createAd);
 router.get("/anuncios", getAds);
+router.get("/anuncios/:id", getAdById);
+router.put("/anuncios/:id", updateAd);
+router.delete("/anuncios/:id", deleteAd);
+router.get("/anuncios/category/:category", getAdsByCategory); // Nueva ruta
 
 module.exports = router;
