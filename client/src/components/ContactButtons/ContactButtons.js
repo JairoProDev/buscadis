5fe01fa4e6a41c3fcca5556bc2bca9db1833c386
Jairo Saul <JairoProDev@gmail.com>
2024-05-28 10:28:31 -0500

    refactor: Update ContactButtons component to improve code organization and readability
    - Refactor ContactButtons.js to use a switch statement for message generation based on category
    - Update message templates for different ad categories
    - Improve code formatting and indentation
    - Remove unnecessary code duplication

--- client/src/components/ContactButtons/ContactButtons.js
+++ client/src/components/ContactButtons/ContactButtons.js
@@ -7,24 +7,24 @@ import "./contactButtons.css";
 function ContactButton({ phone, type, category, url }) {
   const isWhatsApp = type === "whatsapp";
 
-let message;
-switch (category) {
-  case "Autos":
-    message = `¡Hola! Vi su aviso sobre el auto que tiene en venta aquí: ${url} y me interesa. ¿Podría proporcionarme más información, por favor?`;
-    break;
-  case "Empleos":
-    message = `Buen día, encontré su anuncio sobre el empleo que ofrece aquí: ${url} y estoy muy interesado. ¿Podría proporcionarme más detalles al respecto?`;
-    break;
-  case "Inmuebles":
-    message = `Hola, vi su aviso sobre la propiedad aquí: ${url} y me interesa. ¿Podría brindarme más información sobre la misma?`;
-    break;
-  case "Servicios":
-    message = `¡Hola! Vi su anuncio sobre los servicios que ofrece aquí: ${url} y los necesito. ¿Podría proporcionarme más detalles al respecto?`;
-    break;
-  default:
-    message = `Buen día, vi su aviso aquí: ${url} y me interesa, podría proporcionarme más información por favor?`;
-}
-const href = isWhatsApp
+  let message;
+  switch (category) {
+    case "Vehicles":
+      message = `¡Hola! Vi su aviso sobre el Vehículo que tiene en venta aquí: ${url} y me interesa. ¿Podría proporcionarme más información, por favor?`;
+      break;
+    case "Empleos":
+      message = `Buen día, encontré su anuncio sobre el empleo que ofrece aquí: ${url} y estoy muy interesado. ¿Podría proporcionarme más detalles al respecto?`;
+      break;
+    case "Inmuebles":
+      message = `Hola, vi su aviso sobre la propiedad aquí: ${url} y me interesa. ¿Podría brindarme más información por favor?`;
+      break;
+    case "Servicios":
+      message = `¡Hola! Vi su anuncio sobre los servicios que ofrece aquí: ${url} y los necesito. ¿Podría proporcionarme más detalles al respecto?`;
+      break;
+    default:
+      message = `Buen día, vi su aviso aquí: ${url} y me interesa, podría proporcionarme más información por favor?`;
+  }
+  const href = isWhatsApp
     ? `https://wa.me/${phone}?text=${encodeURIComponent(message)}`
     : `tel:${phone}`;
   const icon = isWhatsApp ? faWhatsapp : faPhone;
@@ -49,13 +49,41 @@ function ContactButtons({ phone, phone2, category, url }) {
   return (
     <>
       <div>
-        {phone && <ContactButton phone={phone} type="call" category={category} url={url} />}
-        {phone && <ContactButton phone={phone} type="whatsapp" category={category} url={url} />}
+        {phone && (
+          <ContactButton
+            phone={phone}
+            type="call"
+            category={category}
+            url={url}
+          />
+        )}
+        {phone && (
+          <ContactButton
+            phone={phone}
+            type="whatsapp"
+            category={category}
+            url={url}
+          />
+        )}
       </div>
       <p>BuscAdis</p>
       <div>
-        {phone2 && <ContactButton phone={phone2} type="call" category={category} url={url} />}
-        {phone2 && <ContactButton phone={phone2} type="whatsapp" category={category} url={url} />}
+        {phone2 && (
+          <ContactButton
+            phone={phone2}
+            type="call"
+            category={category}
+            url={url}
+          />
+        )}
+        {phone2 && (
+          <ContactButton
+            phone={phone2}
+            type="whatsapp"
+            category={category}
+            url={url}
+          />
+        )}
       </div>
     </>
   );
