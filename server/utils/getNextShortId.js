2ccfd6bff96e844227a4bef0500adf0cf34839ba
Jairo Saul <JairoProDev@gmail.com>
2024-06-23 13:50:01 -0500

    refactor: Improve error handling in getNextShortId function

--- server/utils/getNextShortId.js
+++ server/utils/getNextShortId.js
@@ -2,12 +2,23 @@
 const Counter = require("../models/counterModel");
 
 const getNextShortId = async () => {
-  const counter = await Counter.findByIdAndUpdate(
-    { _id: "shortId" },
-    { $inc: { sequenceValue: 1 } },
-    { new: true, upsert: true }
-  );
-  return counter.sequenceValue;
+  try {
+    const counter = await Counter.findByIdAndUpdate(
+      { _id: "shortId" },
+      { $inc: { sequenceValue: 1 } },
+      { new: true, upsert: true }
+    );
+    if (!counter) {
+      throw new Error('Counter document not found or failed to update.');
+    }
+    return counter.sequenceValue;
+  } catch (error) {
+    console.error('Error getting next shortId:', error);
+    // Aquí podrías decidir cómo manejar este error. Por ejemplo, podrías:
+    // - Retornar un valor específico que indique un error.
+    // - Lanzar el error para manejarlo más arriba en la cadena de llamadas.
+    // - Intentar recuperar el contador o inicializarlo si es necesario.
+  }
 };
 
 module.exports = getNextShortId;
