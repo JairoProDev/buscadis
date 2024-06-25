cb327175315962878eb209177318a588e5f953f1
Jairo Saul <JairoProDev@gmail.com>
2024-06-25 10:47:52 -0500

    refactor: Update styles for HomePage, Header, and responsive layout
    - Add margin:auto to .right-sidebar in HomePage.css
    - Import ButtomNavBar component in HomePage.js
    - Add ButtomNavBar component to HomePage.js
    - Update transition duration in header.css
    - Adjust margin-bottom in responsive.css
    
    These changes improve the layout and styling of the HomePage, Header, and responsive layout components.

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -14,6 +14,7 @@ import AdModal from "./components/AdModal/AdModal";
 import SocialMedia from "./components/SocialMedia/SocialMedia";
 import UserProfile from "./components/UserProfile/UserProfile";
 import AdsColumn from "./components/AdsColumn/AdsColumn";
+import ButtomNavBar from "./components/BottomNavBar/BottomNavBar";
 
 // Styles
 import "./styles/root.css";
@@ -92,6 +93,7 @@ function HomePage() {
           </div>
           <SocialMedia />
         </div>
+        <ButtomNavBar showForm={showForm} />
       </div>
       <Routes>
         <Route path="/profile" element={<UserProfile />} />
