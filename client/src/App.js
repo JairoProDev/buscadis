6f95f209c15f3708b4c3685344a77f1c6d178ddc
Jairo Saul <JairoProDev@gmail.com>
2024-08-14 09:47:25 -0500

    refactor: Update AuthProvider import path in App.js, create new route for auth

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,17 @@ import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import UserProfile from "./components/UserProfile/UserProfile";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./components/Auth/AuthContext";
+import AuthForm from "./components/Auth/AuthForm";
 
 function App() {
   return (
     <Router>
       <AuthProvider>
         <Routes>
+          <Route path="/auth" element={<AuthForm />} />
           <Route path="/profile" element={<UserProfile />} />
           <Route path="/:adType" element={<HomePage />} />
-          {/* <Route path="*" element={<HomePage />} /> */}
           <Route path="/:adType/:category" element={<HomePage />} />
           <Route path="/:adType/:category/:subcategory" element={<HomePage />} />
           <Route path="/*" element={<HomePage />} />
