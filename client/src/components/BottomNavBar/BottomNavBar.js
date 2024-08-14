23e089668e6dd4d0ede4682848b974e9cc4785d9
Jairo Saul <JairoProDev@gmail.com>
2024-08-14 09:51:12 -0500

    refactor: Add user authentication to BottomNavBar component

--- client/src/components/BottomNavBar/BottomNavBar.js
+++ client/src/components/BottomNavBar/BottomNavBar.js
@@ -1,32 +1,39 @@
-// BottomNavBar.js
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FaMobileAlt, FaSearch, FaRegNewspaper, FaBullhorn, FaUser } from 'react-icons/fa';
 import './bottomNavBar.css';
 import useScroll from '../../hooks/useScroll';
+import { AuthContext } from '../../components/Auth/AuthContext'; 
 
 const navItems = [
   { name: 'Revista', icon: FaRegNewspaper, path: '/' },
-  { name: 'TikShop', icon: FaMobileAlt, path: '/tikshop' },
+  { name: 'TikShop', icon: FaMobileAlt, path: '/' },
   { name: 'Anunciar', icon: FaBullhorn, isButton: true },
   { name: 'Buscar', icon: FaSearch, isSearch: true },
-  { name: 'Perfil', icon: FaUser, path: 'https://wa.me/937054328', isExternal: true }
+  { name: 'Perfil', icon: FaUser }
 ];
 
 function BottomNavBar({ showForm, searchInputRef }) {
   const [activeItem, setActiveItem] = useState(navItems[0].name);
   const isHidden = useScroll();
+  const { user } = useContext(AuthContext); 
+  const navigate = useNavigate();
 
   const handleClick = (item) => {
     setActiveItem(item.name);
-    console.log('searchInputRef in BottomNavBar:', searchInputRef); // Log para verificar la referencia
-    if (item.isSearch) {
-      if (searchInputRef && searchInputRef.current) {
-        searchInputRef.current.focus(); // Enfoca el campo de búsqueda del encabezado
-      } else {
-        console.error("searchInputRef.current is undefined");
-      }
-    } else if (item.isButton) {
+
+    if (item.isSearch && searchInputRef?.current) {
+      searchInputRef.current.focus();
+    } else if (item.isButton && showForm) {
       showForm();
+    } else if (item.name === 'Perfil') {
+      if (user) {
+        navigate('/profile'); 
+      } else {
+        navigate('/auth'); 
+      }
+    } else {
+      navigate(item.path); 
     }
   };
 
@@ -34,34 +41,14 @@ function BottomNavBar({ showForm, searchInputRef }) {
     <div className={`bottom-nav ${isHidden ? 'hidden' : ''}`}>
       {navItems.map((item) => {
         const Icon = item.icon;
-        if (item.isSearch) {
-          return (
-            <div key={item.name} className="bottom-nav-item" onClick={() => handleClick(item)}>
-              <div className={`nav-item-container ${activeItem === item.name ? 'active' : ''}`}>
-                <Icon className="bottom-nav-icon" />
-              </div>
-              <div className="bottom-nav-text">{item.name}</div>
+        return (
+          <div key={item.name} className="bottom-nav-item" onClick={() => handleClick(item)}>
+            <div className={`nav-item-container ${activeItem === item.name ? 'active' : ''}`}>
+              <Icon className="bottom-nav-icon" />
             </div>
-          );
-        } else if (item.isExternal) {
-          return (
-            <a href={item.path} target="_blank" rel="noopener noreferrer" key={item.name} className="bottom-nav-item">
-              <div className={`nav-item-container ${activeItem === item.name ? 'active' : ''}`}>
-                <Icon className="bottom-nav-icon" />
-              </div>
-              <div className="bottom-nav-text">{item.name}</div>
-            </a>
-          );
-        } else {
-          return (
-            <div key={item.name} className="bottom-nav-item" onClick={() => handleClick(item)}>
-              <div className={`nav-item-container ${activeItem === item.name ? 'active' : ''}`}>
-                <Icon className="bottom-nav-icon" />
-              </div>
-              <div className="bottom-nav-text">{item.name}</div>
-            </div>
-          );
-        }
+            <div className="bottom-nav-text">{item.name}</div>
+          </div>
+        );
       })}
     </div>
   );
