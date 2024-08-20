b14255af013f4481089da744faffb4cff023977d
Jairo Saul <JairoProDev@gmail.com>
2024-08-20 12:20:57 -0500

    refactor: Update AdList component to handle empty ad list and display a message

--- client/src/components/AdList/AdList.js
+++ client/src/components/AdList/AdList.js
@@ -4,24 +4,30 @@ import "./adList.css";
 import { Link } from "react-router-dom";
 
 function AdList({ anuncios, setSelectedAd }) {
+  console.log("Anuncios recibidos en AdList:", anuncios); // Verifica si los anuncios se están pasando correctamente
+
   return (
     <div className="ads-container">
       <ul id="ad-list" style={{ listStyleType: "none" }}>
-        {anuncios.map((anuncio, index) => (
-          <li key={anuncio._id} className={`ad-size-${anuncio.size || "normal"}`}>
-            <Link
-              to={`/${anuncio.adType}/${anuncio.category}/${anuncio.subCategory}/${anuncio._id}`}
-              style={{ textDecoration: "none", color: "inherit" }}
-              onClick={() => setSelectedAd(anuncio)} // Asegurarse de que se selecciona el anuncio al hacer clic
-            >
-              <AdCard
-                anuncio={anuncio}
-                number={index + 1}
-                setSelectedAd={setSelectedAd}
-              />
-            </Link>
-          </li>
-        ))}
+        {anuncios.length > 0 ? (
+          anuncios.map((anuncio, index) => (
+            <li key={anuncio._id} className={`ad-size-${anuncio.size || "normal"}`}>
+              <Link
+                to={`/${anuncio.adType}/${anuncio.category}/${anuncio.subCategory}/${anuncio._id}`}
+                style={{ textDecoration: "none", color: "inherit" }}
+                onClick={() => setSelectedAd(anuncio)} // Asegurarse de que se selecciona el anuncio al hacer clic
+              >
+                <AdCard
+                  anuncio={anuncio}
+                  number={index + 1}
+                  setSelectedAd={setSelectedAd}
+                />
+              </Link>
+            </li>
+          ))
+        ) : (
+          <p>No se encontraron anuncios</p>
+        )}
       </ul>
     </div>
   );
