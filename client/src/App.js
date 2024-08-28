1e64a69b4b5c2767cb89659362b09e83300aa312
Jairo Saul <JairoProDev@gmail.com>
2024-08-28 07:56:40 -0500

    refactor: Update route paths in App.js for dynamic adType, category, and subcategory

--- client/src/App.js
+++ client/src/App.js
@@ -13,9 +13,9 @@ function App() {
           <Route path="/auth/login" element={<AuthForm isLoginForm={true} />} />
           <Route path="/auth/register" element={<AuthForm isLoginForm={false} />} />
           <Route path="/profile" element={<UserProfile />} />
-          <Route path="/:adType" element={<HomePage />} />
-          <Route path="/:adType/:category" element={<HomePage />} />
-          <Route path="/:adType/:category/:subcategory" element={<HomePage />} />
+          <Route path="/:adType/*" element={<HomePage />} />
+          <Route path="/:adType/:category/*" element={<HomePage />} />
+          <Route path="/:adType/:category/:subcategory/*" element={<HomePage />} />
           <Route path="/*" element={<HomePage />} />
         </Routes>
       </AuthProvider>
