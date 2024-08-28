78508a40df57d15c558df204d8c6ae171d5b5e0b
Jairo Saul <JairoProDev@gmail.com>
2024-08-28 08:08:49 -0500

    refactor: Improve AdList component to handle empty ad list and display a message
    The AdList component now checks if the 'anuncios' array is empty and displays a message when no ads are available to show. This provides a better user experience by informing them when there are no ads to browse. Additionally, the useEffect hook has been added to log the received ads and their count for debugging purposes.

--- client/src/components/AdList/AdList.js
+++ client/src/components/AdList/AdList.js
@@ -1,27 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AdCard from "../AdCard/AdCard";
 import "./adList.css";
 import { Link } from "react-router-dom";
 
 function AdList({ anuncios, setSelectedAd }) {
+  useEffect(() => {
+    console.log("Anuncios recibidos en AdList:", anuncios);
+    console.log("Cantidad de anuncios:", anuncios.length);
+  }, [anuncios]);
+
   return (
     <div className="ads-container">
       <ul id="ad-list" style={{ listStyleType: "none" }}>
-        {anuncios.map((anuncio, index) => (
-          <li key={anuncio._id} className={`ad-size-${anuncio.size || "normal"}`}>
-            <Link
-              to={`/${anuncio.adType}/${anuncio.category}/${anuncio.subCategory}/${anuncio._id}`}
-              style={{ textDecoration: "none", color: "inherit" }}
-              onClick={() => setSelectedAd(anuncio)} // Asegurarse de que se selecciona el anuncio al hacer clic
-            >
-              <AdCard
-                anuncio={anuncio}
-                number={index + 1}
-                setSelectedAd={setSelectedAd}
-              />
-            </Link>
-          </li>
-        ))}
+        {anuncios.length > 0 ? (
+          anuncios.map((anuncio, index) => (
+            <li key={anuncio._id} className={`ad-size-${anuncio.size || "normal"}`}>
+              <Link
+                to={`/${anuncio.adType}/${anuncio.category}/${anuncio.subCategory}/${anuncio._id}`}
+                style={{ textDecoration: "none", color: "inherit" }}
+                onClick={() => setSelectedAd(anuncio)}
+              >
+                <AdCard
+                  anuncio={anuncio}
+                  number={index + 1}
+                  setSelectedAd={setSelectedAd}
+                />
+              </Link>
+            </li>
+          ))
+        ) : (
+          <p>No se encontraron anuncios para mostrar.</p>
+        )}
       </ul>
     </div>
   );
