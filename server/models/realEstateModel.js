bea9634dc2cc38e642ae7c97e7775547937d7741
Jairo Saul <JairoProDev@gmail.com>
2024-08-27 23:40:37 -0500

    refactor: Create separate controllers

---
+++ server/models/realEstateModel.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const realEstateSchema = new mongoose.Schema(
+  {
+    category: { type: String, required: false, trim: true },
+    subCategory: { type: String, trim: true },
+    title: { type: String, required: true, trim: true, maxlength: 100 },
+    description: { type: String, required: true, trim: true, maxlength: 1800 },
+    location: { type: String, trim: true },
+    phone: {
+      type: String,
+      trim: true,
+      validate: {
+        validator: function (v) {
+          return /^(\+51)?\d{9}$/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid phone number!`,
+      },
+    },
+    phone2: {
+      type: String,
+      trim: true,
+      validate: {
+        validator: function (v) {
+          return !v || /^(\+51)?\d{9}$/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid phone number!`,
+      },
+    },
+    amount: { type: String },
+    email: {
+      type: String,
+      trim: true,
+      validate: {
+        validator: function (v) {
+          return v ? validator.isEmail(v) : true;
+        },
+        message: (props) => `${props.value} is not a valid email address!`,
+      },
+    },
+    size: { type: String, enum: ["miniature", "normal", "long", "large", "giant"], default: "normal" },
+    images: [String],
+  },
+  { timestamps: true }
+);
+
+const RealEstate = mongoose.model("RealEstate", realEstateSchema);
+
+module.exports = RealEstate;
