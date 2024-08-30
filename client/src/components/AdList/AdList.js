80d3a7bc94dbcbc99c5100e974c02270138b721a
Jairo Saul <JairoProDev@gmail.com>
2024-08-30 16:48:41 -0500

    Manage the logic in adlist, changing some code that worked well before in the past

--- client/src/components/AdList/AdList.js
+++ client/src/components/AdList/AdList.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import AdCard from "../AdCard/AdCard";
+import Modal from "../Modal/Modal";  // Asegúrate de crear este componente
 import "./adList.css";
-import { Link } from "react-router-dom";
 
-function AdList({ anuncios, setSelectedAd }) {
+function AdList({ anuncios }) {
   const [loading, setLoading] = useState(true);
+  const [selectedAd, setSelectedAd] = useState(null);
 
   useEffect(() => {
     if (anuncios.length > 0) {
@@ -16,6 +17,14 @@ function AdList({ anuncios, setSelectedAd }) {
     console.log("Cantidad de anuncios:", anuncios.length);
   }, [anuncios]);
 
+  const handleAdClick = (anuncio) => {
+    setSelectedAd(anuncio);  // Al hacer clic en un anuncio, lo seleccionamos para mostrar en el modal
+  };
+
+  const closeModal = () => {
+    setSelectedAd(null);  // Esta función cierra el modal
+  };
+
   return (
     <div className="ads-container">
       <ul id="ad-list" style={{ listStyleType: "none" }}>
@@ -24,23 +33,26 @@ function AdList({ anuncios, setSelectedAd }) {
         ) : anuncios.length > 0 ? (
           anuncios.map((anuncio, index) => (
             <li key={anuncio._id} className={`ad-size-${anuncio.size || "normal"}`}>
-              <Link
-                to={`/${anuncio.adType}/${anuncio.category}/${anuncio.subCategory}/${anuncio._id}`}
-                style={{ textDecoration: "none", color: "inherit" }}
-                onClick={() => setSelectedAd(anuncio)}
+              <div
+                onClick={() => handleAdClick(anuncio)}  // Ahora manejamos el clic para abrir el modal
+                style={{ cursor: "pointer" }}
               >
                 <AdCard
                   anuncio={anuncio}
                   number={index + 1}
                   setSelectedAd={setSelectedAd}
                 />
-              </Link>
+              </div>
             </li>
           ))
         ) : (
           <p>No se encontraron anuncios para mostrar.</p>
         )}
       </ul>
+      
+      {selectedAd && (
+        <Modal anuncio={selectedAd} onClose={closeModal} />
+      )}
     </div>
   );
 }
