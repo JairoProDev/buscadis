aecfe01dcd6cc66858b6298e41e9358bcf588c60
Jairo Saul <JairoProDev@gmail.com>
2024-11-16 10:08:21 -0500

    Refactor: Update WhatsApp link text in SocialMedia.js for beta version promotion of our IA

--- client/src/components/SocialMedia/SocialMedia.js
+++ client/src/components/SocialMedia/SocialMedia.js
@@ -52,7 +52,7 @@ const SocialMedia = () => {
         </li> */}
         <li className="icon whatsapp">
           <span className="tooltip">WhatsApp</span>
-          <a href="https://api.whatsapp.com/send?phone=51937054328&text=Hola%2C%20vengo%20de%20la%20app%2C%20quiero%20unirme%20a%20la%20comunidad%20de%20WhatsApp%20y%20obtener%20m%C3%A1s%20beneficios" target="_blank" rel="noreferrer">
+          <a href="https://api.whatsapp.com/send?phone=51937054328&text=Hola%2C%20vengo%20de%20la%20app%2C%20quiero%20unirme%20a%20la%20version%20beta%20de%20ADIS%20y%20probar%20su%inteligencia%artificial" target="_blank" rel="noreferrer">
             <FontAwesomeIcon icon={faWhatsapp} />
           </a>
         </li>
